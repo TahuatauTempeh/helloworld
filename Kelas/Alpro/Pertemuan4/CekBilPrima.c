53fafa547b0ca9e75b12be056c9bee1fae7fd002
Akka <akka.m4a@gmail.com>
2025-03-09 19:06:08 +0700

    22

---
+++ Kelas/Alpro/Pertemuan4/CekBilPrima.c
@@ -0,0 +1,15 @@
+/*Nama File 	: CekBilPrima.c*/
+/*Deskripsi 	: cek bilangan prima apa tidak*/
+/*Pembuat   	: Azka Aqylla Maulana-24060124140195*/
+/*Tgl Pembuatan	: 04/03/2025*/
+
+#include <stdio.h> /*header file*/
+
+/*Program Utama*/
+int main()
+{   /*Kamus*/
+  
+    /*Algoritma*/
+  
+    return 0;
+}
