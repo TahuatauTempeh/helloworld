d5d2e8da0cab043b040ef3776587d25dee1e4268
Akka <akka.m4a@gmail.com>
2025-03-09 09:17:56 +0700

    git push

---
+++ Kelas/Alpro/Pertemuan4/JumDeret.c
@@ -0,0 +1,25 @@
+/*Nama File 	: JumDeret.c*/
+/*Deskripsi 	: menghitung jumlah total dari nth deret*/
+/*Pembuat   	: Azka Aqylla Maulana-24060124140195*/
+/*Tgl Pembuatan	: 03/03/25*/
+
+#include <stdio.h> /*header file*/
+
+/*Program Utama*/
+int main()
+{   /*Kamus*/
+    float N;    /*brp banyak bilangan yg mau ditambah dari angka 1 dst. n>0*/
+   
+    /*Algoritma*/
+    printf("Berapa kali penjumlahan: \n");
+    scanf("%f", &N);
+
+    while (N )
+    {
+        /* code */
+    }
+    
+
+
+    return 0;
+}
