0b0b975ebf992446d20a9c3f2823b5e2f439efec
Akka <akka.m4a@gmail.com>
2025-03-23 17:17:21 +0700

    222222

---
+++ Kelas/Alpro/Pertemuan5/Sub/1.c
@@ -0,0 +1,18 @@
+/*Nama File 	: 1.c*/
+/*Deskripsi 	: tukar nilai 2 bilangan*/
+/*Pembuat   	: Azka Aqylla Maulana 2406012414195*/
+
+
+
+#include <stdio.h> /*header file*/
+
+
+
+/*Program Utama*/
+int main()
+{   /*Kamus*/
+  
+    /*Algoritma*/
+  
+    return 0;
+}
---
+++ personaltesting/gedagedi/number1to10.c
@@ -0,0 +1,22 @@
+/*Number selector from 1 - 10*/
+
+#include <stdio.h>
+
+void arrayofnumbers()   /*buat ambil angka yg ada di array sesuai permintaan user*/
+{
+    /*Kamus*/
+    int arraytennumbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    /*algoritma*/
+    for (int i = 0; i < 11; i++)
+    {
+        /* code */
+    }
+    
+}
+
+int main()
+{
+    printf("Masukkan urutan dari 1 - 10: \n");
+
+}
